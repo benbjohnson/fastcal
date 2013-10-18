15133a68b3430560807709da57fb973ce7f368f9
Tom Burns <tom.burns@jadedpixel.com>
2013-10-18 15:46:55 -0400

    implement the baseline, go standard library

---
+++ fastcal.go
@@ -0,0 +1,38 @@
+package fastcal
+
+import (
+  "time"
+)
+
+type Fastcal interface {
+  Set(int64)
+  Hour() int
+  Day() int
+  Month() int
+  Year() int
+}
+
+type GoStdlib struct {
+  timeobj time.Time
+}
+
+func (t *GoStdlib) Set(epoch int64) {
+  t.timeobj = time.Unix(epoch, 0)
+}
+
+func (t *GoStdlib) Hour() int {
+  return t.timeobj.Hour()
+}
+
+func (t *GoStdlib) Day() int {
+  return t.timeobj.Day()
+}
+
+func (t *GoStdlib) Month() int {
+  return (int)(t.timeobj.Month())
+}
+
+func (t *GoStdlib) Year() int {
+  return t.timeobj.Year()
+}
+
---
+++ fastcal_benchmark_test.go
@@ -0,0 +1,23 @@
+package fastcal
+
+import (
+  "testing"
+)
+
+// 10MM events per second, so the entire query has 100ns.
+var epoch = 1287429458
+
+// 300ns to convert
+func BenchmarkStdlibEpochToCalendar(b *testing.B) {
+  t := GoStdlib{}
+
+  for i := 0; i < b.N; i++ {
+    t.Set((int64)(epoch))
+    _ = t.Hour()
+    _ = t.Day()
+    _ = t.Month()
+    _ = t.Year()
+  }
+}
+
+
